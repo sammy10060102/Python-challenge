859c226b728d2a860d4bf5e0dcad8ac947b35010
Phuong Han <phuong.han1990@gmail.com>
2021-05-28 00:22:03 +1000

    finished PyPoll assignment and readme

---
+++ PyPoll/README.md
@@ -0,0 +1,12 @@
+# This script does the following:
+* Displays the total number of votes
+* Displays the complete list of candidates
+* Displays the percentage of votes per candidate
+* displays the total number of votes per candidate
+* Displays the winner of the Election.
+
+## Election results output to analysis folder
+
+## To Run Script
+* Ensure you are in home directory where main.py lives
+* run `python main.py`
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -10,13 +10,70 @@ with open(election_data) as csv_file:
 
     csv_header = next(csv_reader)
 
-    count_votes = 0
+    total_votes = 0
+
+    percentage = 0
+    votes_per_candidate = []
+    percentage_of_votes_per_candidate = []
+    candidates_list = []
+    
+    
 
     #count total votes
     for row in csv_reader:
-        count_votes += 1
 
-    print ("Election Results")
-    print ("---------------------------------")
-    print ("Total Votes:  "  + str(count_votes))
-    
+        total_votes += 1
+
+        candidate = row[2]
+        
+        if not candidates_list.__contains__(candidate):
+            # add new candidate to candidate list
+            candidates_list.append(candidate)
+            # add new index to array of votes per candidate
+            votes_per_candidate.append(1)
+        else:
+            #get index of candidate
+            candidate_index = candidates_list.index(candidate)
+            #add vote to candidate 
+            votes_per_candidate[candidate_index] += 1
+
+    for votes in votes_per_candidate:
+        #calculate the percentage of votes per candidate
+        percentage = (votes / total_votes) * 100
+        percentage_of_votes_per_candidate.append(percentage)
+
+    #check highest vote
+    highestCheck = votes_per_candidate[0]
+    winning_candidate = ""
+    winner = max(votes_per_candidate)
+    #get index of highest vote
+    index = 0
+    index = votes_per_candidate.index(winner)
+    #allocate winning candidate
+    winning_candidate = candidates_list[index]
+
+    line1 = "Election Results"
+    line2 = "---------------------------------"
+    line3 = "Total Votes:  "  + str(total_votes)
+    line4 = "---------------------------------"
+    line5 = "---------------------------------"
+    line6 = "Winner: " + str(winning_candidate)
+    line7 = "---------------------------------"
+
+    print(line1)
+    print(line2)
+    print(line3)
+    print(line4)
+    for index in range(len(candidates_list)):
+        print(f"{candidates_list[index]}: {round(percentage_of_votes_per_candidate[index], 4)}% ({votes_per_candidate[index]})")
+    print(line5) 
+    print(line6)
+    print(line7)
+
+    # open a text file to write to 
+    result = open("analysis/result.txt", "w")
+
+    result.write('{}\n{}\n{}\n{}'.format(line1,line2,line3,line4))
+    for index in range(len(candidates_list)):
+        result.write('\n{}'.format(f"{candidates_list[index]}: {round(percentage_of_votes_per_candidate[index], 5)}% ({votes_per_candidate[index]})"))
+    result.write('\n{}\n{}\n{}'.format(line5,line6, line7))
