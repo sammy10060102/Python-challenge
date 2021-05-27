95d1eb5124223f43fe0317984be3d1fb556e4085
Phuong Han <phuong.han1990@gmail.com>
2021-05-27 22:32:45 +1000

    Count total number of votes

---
+++ PyBank/README.md
@@ -0,0 +1,3 @@
+This script reads in CSV data that contains profit/loss statement for everymonth in a set period and outputs the following date to a result.txt within the analysis folder.
+
+    
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,7 +5,18 @@ election_data = os.path.join("Resources", "election_data.csv")
 
 #open and read csv
 with open(election_data) as csv_file:
+
     csv_reader = csv.reader(csv_file, delimiter=",")
 
+    csv_header = next(csv_reader)
+
+    count_votes = 0
+
+#count total votes
     for row in csv_reader:
-        print("csv file display " + str(row))
+        count_votes += 1
+
+    print ("Election Results")
+    print ("---------------------------------")
+    print ("Total Votes:  "  + str(count_votes))
+    
